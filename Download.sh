10aace2d522f754c4d5592323d4c9537e61c3030
Pradeep Chauhan <91078821+pradeep-lit@users.noreply.github.com>
2024-03-01 14:10:09 +0530

    Update Download.sh

--- Download.sh
+++ Download.sh
@@ -2,6 +2,7 @@ git clone -b uvite https://github.com/basamaryan/android_kernel_xiaomi_sm6150.gi
 mkdir toolchain
 cd toolchain
 wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-14.0.0_r28/clang-r487747c.tar.gz
+mkdir clang
 tar -xvf clang-r487747c.tar.gz -C /root/toolchain/clang/
 cd android_kernel_xiaomi_sm6150
 git fetch origin lineage-20
